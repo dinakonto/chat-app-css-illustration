55ceaea017c9fd65ad32299528ba105b003321cb
Dina Konto <dina.konto@gmail.com>
2020-10-13 23:39:54 +1100

    Create array to hold message objects

---
+++ js/chat.js
@@ -0,0 +1,47 @@
+const messages = [
+  {
+    msg: 'That sounds great. I’d be happy with that.',
+    from: 'sam',
+    type: 'text'
+  },
+  {
+    msg: 'Could you send over some pictures of your dog, please?',
+    from: 'sam',
+    type: 'text'
+  },
+  {
+    msg: '3 images go here',
+    from: 'me',
+    type: 'image'
+
+  },
+  {
+    msg: 'Here are a few pictures. She’s a happy girl!',
+    from: 'me',
+    type: 'text'
+
+  },
+  {
+    msg: 'Can you make it?',
+    from: 'me',
+    type: 'text'
+  },
+  {
+    msg: 'She looks so happy! The time we discussed works. How long shall I take her out for?',
+    from: 'sam',
+    type: 'text'
+
+  },
+  {
+    msg: '30 minute walk',
+    price: 29,
+    from: 'sam',
+    type: 'choice'
+  },
+  {
+    msg: '1 hour walk',
+    price: 49,
+    from: 'sam',
+    type: 'choice'
+  }
+]
