4c0778c955d2613a80290a92b90d217cd5a7e457
Dina Konto <dina.konto@gmail.com>
2020-10-14 22:13:27 +1100

    Display images in chat

--- js/chat.js
+++ js/chat.js
@@ -15,9 +15,9 @@ $(function() {
     {
       msg: '',
       src: [
-        '../../images/dog-image-1.jpg',
-        '../../images/dog-image-2.jpg',
-        '../../images/dog-image-3.jpg'
+        '/images/dog-image-1.jpg',
+        '/images/dog-image-2.jpg',
+        '/images/dog-image-3.jpg'
       ],
       from: 'me',
       type: 'image'
@@ -53,14 +53,57 @@ $(function() {
     }
   ];
 
-
+  // Loop through messages and display each one
   let delay = 1000;
 
-  messages.forEach(function(value, index) {
+  messages.forEach(function(msg, index) {
     setTimeout(function() {
-      let msgBubbleHTML = '<li class="msg from-' + value.from + ' type-' + value.type + '"><div class="msg-content"><p>' + value.msg + '</p></div></div>'
+      let msgBubbleHTML;
+      // Add basic building blocks (All)
+      msgBubbleHTML = '<li class="msg from-' + msg.from + ' type-' + msg.type + '"><div class="msg-content">';
+
+      if (msg.type === 'image') {
+        // Add images (Images)
+        let imgArray = msg.src;
+        imgArray.forEach(function(src, index) {
+          msgBubbleHTML += '<img src=".' + src + '">'
+        });
+
+      } else {
+        // Add text (Text, choice)
+        msgBubbleHTML += '<p>' + msg.msg + '</p>'
+      }
+
+      // End block (All)
+      msgBubbleHTML += '</div></div>'
+      // Add to DOM (All)
       document.querySelector("#msg-area .msg-list").innerHTML += msgBubbleHTML;
     }, delay);
     delay += 1000;
   });
-})
+
+
+
+
+});
+
+  //
+  // messages.forEach(function(msg, index) {
+  //   setTimeout(function() {
+  //     let msgBubbleHTML = '<li class="msg from-' + msg.from + ' type-' + msg.type + '"><div class="msg-content">';
+  //
+  //     // If it's images
+  //
+  //
+  //
+  //     // If it's text
+  //     // else {
+  //       msgBubbleHTML += '<p>' + msg.msg + '</p>'
+  //     // }
+  //     msgBubbleHTML += '</div></div>'
+  //     // Add it to the DOM
+  //     document.querySelector("#msg-area .msg-list").innerHTML += msgBubbleHTML;
+  //   })
+  //   }, delay);
+  //   delay += 1000;
+  //   });
